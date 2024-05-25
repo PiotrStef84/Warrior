968a219c7e8e3ca6075e384dc9298420440ab78e
Piotr <xmestre@wp.pl>
2024-05-25 10:12:00 +0100

    Creating a GamePanel, adding some settings

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -48,5 +48,15 @@ public class GamePanel extends JPanel implements Runnable{
     public void paintComponent(Graphics g){
 
         super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(100, 100, tileSize, tileSize);
+
+        g2.fillRect(200, 200, tileSize, tileSize);
+
+        // Dispose of this graphics context and release any system resources that it's using
+        g2.dispose(); // good practice to save some memory
     }
 }
