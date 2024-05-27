b59e0b2997165864ef4d12e42afc616e4ad99f14
Piotr <xmestre@wp.pl>
2024-05-27 09:44:51 +0100

    starting on a key input

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -4,6 +4,9 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
+
+    public boolean upPressed, downPressed, leftPressed, rightPressed;
+
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -11,7 +14,20 @@ public class KeyHandler implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
 
+        if(code ==KeyEvent.VK_W){
+            upPressed = true;
+        }
+        if(code ==KeyEvent.VK_S){
+            downPressed = true;
+        }
+        if(code ==KeyEvent.VK_A){
+            leftPressed = true;
+        }
+        if(code ==KeyEvent.VK_D){
+            rightPressed = true;
+        }
     }
 
     @Override
